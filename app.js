fca65af5d6498d8ae532e70f0c65f0d9a82260a3
Patrick Hrabos <phrabos@gmail.com>
2020-12-03 22:14:44 -0500

    add stying and resets input boxes when submit button clicked

--- app.js
+++ app.js
@@ -3,36 +3,75 @@ const addSubmit = document.getElementById("submit-add");
 const subtractSubmit = document.getElementById("submit-subtract");
 const multiplySubmit = document.getElementById("submit-multiply");
 const divideSubmit = document.getElementById("submit-divide");
+const resAdd = document.getElementById('result-add');
+const resSub = document.getElementById('result-subtract');
+const resMult = document.getElementById('result-multiply');
+const resDiv = document.getElementById('result-divide');
 
 
 
 //event listeners
-addSubmit.addEventListener("click", (event) => {
-    event.preventDefault();
-    const addResult = (parseInt(addInput1.value)) + parseInt((addInput2.value));
-    const addAnswer = document.getElementById("result-add");
-    addAnswer.innerText = addResult;
+addSubmit.addEventListener("click", () => {
+    //calculate value
+    const addResult = Number(addInput1.value) + Number(addInput2.value);
+    //round result to 2 decimal places
+    const roundAddResult = addResult.toFixed(2);
+    //display the rounded result in the result box
+    resAdd.innerText = roundAddResult;
+    //reset input/output boxes
+    addInput1.value="";
+    addInput2.value="";
+    resSub.innerText="";
+    resMult.innerText="";
+    resDiv.innerText="";
 
 });
 
-subtractSubmit.addEventListener("click", (event) => {
-    event.preventDefault();
-    const subtractResult = (parseInt(subInput1.value)) - parseInt((subInput2.value));
-    const subAnswer = document.getElementById("result-subtract");
-    subAnswer.innerText = subtractResult;
+subtractSubmit.addEventListener("click", () => {
+    //calculate value
+    const subtractResult = Number(subInput1.value)- Number(subInput2.value);
+    //round result to 2 decimal places
+    const roundSubResult = subtractResult.toFixed(2);
+    //display the rounded result in the result box
+    resSub.innerText = roundSubResult;
+    //reset input/output boxes
+    subInput1.value = "";
+    subInput2.value = "";
+    resAdd.innerText="";
+    resMult.innerText="";
+    resDiv.innerText="";
 });
 
-multiplySubmit.addEventListener("click", (event) => {
-    event.preventDefault();
-    const multiplyResult = (parseInt(multInput1.value)) * parseInt((multInput2.value));
-    const multAnswer = document.getElementById("result-multiply");
-    multAnswer.innerText = multiplyResult;
+multiplySubmit.addEventListener("click", () => {
+    //calculate value
+    const multiplyResult = Number(multInput1.value) * Number(multInput2.value);
+    //round result to 2 decimal places
+    const roundMultResult = multiplyResult.toFixed(2);
+    //display the rounded result in the result box
+    resMult.innerText = roundMultResult;
+    //reset input/output boxes
+    multInput1.value = "";
+    multInput2.value = "";
+    resSub.innerText="";
+    resAdd.innerText="";
+    resDiv.innerText="";
 });
 
-divideSubmit.addEventListener("click", (event) => {
-    event.preventDefault();
-    const divideResult = (parseInt(divInput1.value)) / parseInt((divInput2.value));
-    const divAnswer = document.getElementById("result-divide");
-    divAnswer.innerText = divideResult;
+divideSubmit.addEventListener("click", () => {
+    //calculate value
+    const divideResult = Number((divInput1.value)) / Number((divInput2.value));
+    //round result to 2 decimal places
+    const roundDivResult = divideResult.toFixed(2);
+    //display the rounded result in the result box
+    resDiv.innerText = roundDivResult;
+    //reset input/output boxes
+    divInput1.value = "";
+    divInput2.value = "";
+    resSub.innerText="";
+    resMult.innerText="";
+    resAdd.innerText="";
+
 });
+
+
 

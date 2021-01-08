077dc840c4365f37162bdcd785c80f9fadbca6ee
Patrick Hrabos <phrabos@gmail.com>
2021-01-08 16:56:58 -0500

    add testfunctions.js to create functions for use in example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,107 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+ import { sumFunction, subFunction, multFunction, divFunction } from '../testfunctions.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should return 12 when provided 8 and 4', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 12;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = sumFunction(8,4);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('it should return 50 when provided 32 and 16', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 50;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = sumFunction(34,16);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('it should return 5 when provided 10 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subFunction(10,5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('it should return 24 when provided 26 and 50', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -24;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subFunction(26,50);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('it should return 35 when provided 7 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 35;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multFunction(7,5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('it should return -27 when provided -9 and 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -27;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multFunction(-9,3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('it should return 2 when provided 6 and 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 2;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divFunction(6,3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('it should return -20 when provided -100 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -20;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divFunction(-100,5);
 
     //Expect
     // Make assertions about what is expected versus the actual result
---
+++ testfunctions.js
@@ -0,0 +1,18 @@
+// these functions are not used in the app, they were 
+// created to run tests in example.test.js
+
+export function sumFunction(num1, num2) {
+    return num1 + num2;
+}
+
+export function subFunction(num1, num2) {
+    return num1 - num2;
+}
+
+export function multFunction(num1, num2) {
+    return num1 * num2;
+}
+
+export function divFunction(num1, num2) {
+    return num1 / num2;
+}
